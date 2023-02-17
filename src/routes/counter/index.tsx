9103999a8e0960e440404b7c5e74ff2c5e42a9a1
rubyjane88 <rubyjane88@gmail.com>
2023-02-17 14:35:46 +0100

    added Tailwind style to Counter buttons

--- src/routes/counter/index.tsx
+++ src/routes/counter/index.tsx
@@ -8,6 +8,8 @@ export default component$(() => {
       <CounterChild store={store} />
       <p>
         <button
+          type="button"
+          class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-10 mb-2"
           onClick$={() => {
             console.log("Click Increment");
             store.count++;
@@ -15,7 +17,12 @@ export default component$(() => {
         >
           Increment
         </button>
+      </p>
+
+      <section>
         <button
+          type="button"
+          class="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-10 mb-2"
           onClick$={() => {
             console.log("Click Alert");
             alert("I am an Alert!!");
@@ -23,7 +30,7 @@ export default component$(() => {
         >
           Alert
         </button>
-      </p>
+      </section>
     </div>
   );
 });
